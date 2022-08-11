c574555bf418078b964cb6c3bebc242ee490c4ce
eodero <emmanuelodero808@gmail.com>
2022-08-11 17:11:42 +0300

    renamed attributes, new attribute in form field

--- resources/views/products/sell.blade.php
+++ resources/views/products/sell.blade.php
@@ -10,7 +10,7 @@
         <x-slot name="title">
             What are you selling today?
         </x-slot>
-            <form action="/product" method="POST">
+            <form action="/product" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mt-2">
                     <label for="title" class="text-sm text-gray-500">Product Title</label>
@@ -22,15 +22,15 @@
                 </div>
                 <div class="mt-2">
                     <label for="desc_full" class="text-sm text-gray-500">Add Complete Details Of The Product</label>
-                    <textarea type="text" name="desc-full" id="desc_full" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full"></textarea>
+                    <textarea type="text" name="desc_full" id="desc_full" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full"></textarea>
                 </div>
                 <div class="mt-2">
                     <label for="Price" class="text-sm text-gray-500">Product Price</label>
                     <input type="text" name="price" id="price" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
                 </div>
                 <div class="mt-2">
-                    <label for="img" class="text-sm text-gray-500">Upload Product Image</label>
-                    <input type="file" name="product-img" id="product-img" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
+                    <label for="product_img" class="text-sm text-gray-500">Upload Product Image</label>
+                    <input type="file" name="product_img" id="product_img" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
                 </div>
                 <x-button class="mt-4 w-full justify-center">
                    Post the Product 
