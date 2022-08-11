09e60920377be178b233e1780a88f975c96c8a02
eodero <emmanuelodero808@gmail.com>
2022-08-11 15:55:19 +0300

    created a product model and did the migrations for Products table

---
+++ database/migrations/2022_08_11_104921_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('short_desc');
+            $table->longText('full_desc');
+            $table->string('price');
+            $table->bigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
