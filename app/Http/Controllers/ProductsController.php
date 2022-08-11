e0d97a559f5c8822e9b84cab6473026c9f1da75e
eodero <emmanuelodero808@gmail.com>
2022-08-11 16:07:44 +0300

    created Products controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //store products
+    public function store(Request $request){
+        //validate the inputs
+        //upload the image
+        //Insert data into the products table
+    }
+}
